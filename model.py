b765545785f6029b1f14a58cd8e1a4c4c31dfa40
Ken S. Zhang <sz1851@nyu.edu>
2021-03-31 13:56:34 +0800

    Add files via upload

--- model.py
+++ model.py
@@ -2,6 +2,7 @@
 
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 
 
 class FeatureExtraction(nn.Sequential):
@@ -21,14 +22,42 @@ class FeatureExtraction(nn.Sequential):
         return output
 
 
-class UpSampling(nn.Sequential):
-    def __init__(self, inputSize, outputSize, scale=2):
-        super(UpSampling, self).__init__()
-        self.deConv_A = torch.nn.ConvTranspose2d(inputSize, outputSize, kernel_size=4, stride=2, padding=1, bias=True) 
+class BottleNeck(nn.Sequential):
+    def __init__(self, inputSize, outputSize):
+        super(BottleNeck, self).__init__()
+        self.conv_A = nn.Conv2d(inputSize, outputSize, kernel_size=1, stride=1, padding=0)
         self.relu_A = nn.PReLU()
 
     def forward(self, x):
-        output = self.relu_A(self.deConv_A(x))
+        output = self.relu_A(self.conv_A(x))
+        return output
+
+
+class UpSampling(nn.Sequential):
+    def __init__(self, inputSize, outputSize, scale=2):
+        super(UpSampling, self).__init__()
+        self.scale = scale
+        self.deConv_A = torch.nn.ConvTranspose2d(inputSize, outputSize, kernel_size=4, stride=2, padding=1, bias=True) 
+        self.relu_A = nn.PReLU()
+        self.conv_B = nn.Conv2d(outputSize, outputSize, kernel_size=3, stride=1, padding=1) 
+        self.relu_B = nn.PReLU()
+
+    def forward(self, x):
+        phase_A = self.relu_A(self.deConv_A(x))
+        output = self.relu_B(self.conv_B(phase_A))
+        return output
+
+
+class Interpolation(nn.Sequential):
+    def __init__(self, inputSize, outputSize, scale=2):
+        super(Interpolation, self).__init__()
+        self.scale = scale
+        self.conv_A = nn.Conv2d(inputSize, outputSize, kernel_size=3, stride=1, padding=1) 
+        self.relu_A = nn.PReLU()
+
+    def forward(self, x):
+        phase_A = F.interpolate(x, scale_factor=self.scale, mode='bicubic', align_corners=True)
+        output = self.relu_A(self.conv_A(phase_A))
         return output
 
 
@@ -75,12 +104,44 @@ class Model(nn.Module):
         return output
 
 
+class ModelPlus(nn.Module): 
+    def __init__(self):
+        super(ModelPlus, self).__init__() 
+        # UP
+        self.featEx_A = FeatureExtraction(inputSize=3, outputSize=64, ratio=2)
+        self.Interp_A = Interpolation(inputSize=64, outputSize=128)
+        self.Ups_A = UpSampling(inputSize=64, outputSize=128)
+        self.BottleN_A = BottleNeck(inputSize=128*2, outputSize=128)
+        # UP
+        self.featEx_B = FeatureExtraction(inputSize=128, outputSize=128, ratio=2)
+        self.Interp_B = Interpolation(inputSize=128, outputSize=256)
+        self.Ups_B = UpSampling(inputSize=128, outputSize=256)
+        self.BottleN_B = BottleNeck(inputSize=256*2, outputSize=256)
+
+        self.Distill = Refine(inputSize=256, midSizeList=[128, 64])
+
+    def forward(self, x):
+        phase_A = self.featEx_A(x)
+        phase_B_1 = self.Interp_A(phase_A)
+        phase_B_2 = self.Ups_A(phase_A)
+        concat = torch.cat([phase_B_1, phase_B_2], dim=1)
+        phase_C = self.BottleN_A(concat)
+        phase_D = self.featEx_B(phase_C)
+        phase_E_1 = self.Interp_B(phase_D)
+        phase_E_2 = self.Ups_B(phase_D)       
+        concat = torch.cat([phase_E_1, phase_E_2], dim=1)
+        phase_F = self.BottleN_B(concat)
+        output = self.Distill(phase_F)
+        return output
+
+
+
 if __name__ == '__main__': 
     #model = FeatureExtraction(inputSize=3, outputSize=64, ratio=2)
     #model = UpSampling(inputSize=3, outputSize=64)
     #model = Refine(inputSize=64, midSizeList=[32, 8])
-    model = Model()
-    input = torch.rand(size=(1, 3, 384, 512)) 
+    model = ModelPlus()
+    input = torch.rand(size=(1, 3, 10, 20)) 
     with torch.no_grad():
         out = model(input) 
         print(out.shape)
