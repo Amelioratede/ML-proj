94ea2bc11072371937bd457a009e7bf0767b49bb
Ken S. Zhang <sz1851@nyu.edu>
2021-04-06 12:04:56 +0800

    Add files via upload

---
+++ 4-6/div2k.py
@@ -0,0 +1,69 @@
+# Written by Ken S. Zhang for ML Proj
+
+import os
+from torch.utils.data import DataLoader
+from PIL import Image
+from torch.utils import data
+import numpy as np
+from utils import getValTransforms, getTrainTransforms
+
+
+splits = ["valid", "train"]
+DIV2kBasePath = '/home/shanyizhang/Documents/div2k'
+# Data Available at https://data.vision.ee.ethz.ch/cvl/DIV2K/
+
+
+class DIV2kBase(data.Dataset):
+    def __init__(self, root, split):
+        assert(split in splits)
+        self.split = split
+        self.splitRoot = os.path.join(root, 'DIV2K_{}_HR'.format(split))
+        self.imgs = os.listdir(self.splitRoot)
+
+    def __len__(self):
+        return len(self.imgs)
+
+    def __getitem__(self, index):
+        return self.imgs[index]
+
+
+class DIV2k(DIV2kBase):
+    def __init__(self, root, split, scale, reduction=1, transforms=None):
+        super().__init__(root, split)
+        self.scale = scale
+        self.reduction = reduction
+        self.transforms = transforms
+
+    def __getitem__(self, index):
+        imgName = super().__getitem__(index)
+        imgPath = os.path.join(self.splitRoot, imgName)
+        img = Image.open(imgPath)
+        width = int(img.size[0] / self.reduction / self.scale) * self.scale
+        height = int(img.size[1] / self.reduction / self.scale) * self.scale
+        imgOriginal = img.resize((width, height), Image.BICUBIC)
+        imgLow = imgOriginal.resize((width // self.scale, height // self.scale), Image.BICUBIC)
+        del img
+        sample = {'groundTruth': imgOriginal, 'input': imgLow}
+        if self.transforms is not None:
+            sample = self.transforms(sample)
+        return sample
+
+
+def validationData(scale, reduction):
+    valLoader = DIV2k(root=DIV2kBasePath, split='valid', scale=scale, reduction=reduction, transforms=getValTransforms())
+    return DataLoader(valLoader, batch_size=1, shuffle=False)
+
+
+def trainData(scale, reduction):
+    trainLoader = DIV2k(root=DIV2kBasePath, split='train', scale=scale, reduction=reduction, transforms=getTrainTransforms(scale=scale))
+    return DataLoader(trainLoader, batch_size=1, shuffle=True)
+
+
+if __name__ == '__main__':
+    dset = DIV2k(root=DIV2kBasePath, split='train', scale=4, transforms=None)
+    for i in range(len(dset)):
+        instance = dset[i]
+        gt = instance['groundTruth']
+        low = instance['input']
+        #gt.save("gt.png")
+        #low.save("low.png")
---
+++ 4-6/eval.py
@@ -0,0 +1,94 @@
+# Written by Ken S. Zhang for ML Proj
+
+import argparse
+import os
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from datetime import datetime
+from model import Model
+from div2k import trainData, validationData
+from utils import AvgMeter, ToNumpy, PSNR
+
+
+LOG = open('./evallog.txt','a')
+ckptDir = './ckpt'
+
+
+def logPrint(string): 
+    LOG.write(string + "\n") 
+    print(string)
+
+
+def main():
+    parser = argparse.ArgumentParser(description='Single Image Super-resolution <Evaluation>')
+    parser.add_argument('-s', '--scale', default=4, type=int, help='Upsampling scale of super-resolution') 
+    parser.add_argument('-c', '--ckpt', default=None, type=int, help='The selected checkpoint for evaluation')
+    parser.add_argument('-r', '--reduction', default=3, type=int, help='Reduction of input size') 
+    args = parser.parse_args()
+
+    # Hyperparams   
+    scale = args.scale
+    ckptPt = args.ckpt
+    reduction = args.reduction
+
+    # Time & Config
+    currTime = datetime.now().strftime("%m/%d/%Y, %H:%M:%S")
+    logPrint("\n-------- Time: {} --------".format(currTime))
+    logPrint("Config: Scale-{}".format(scale))
+    
+    # Load Model
+    model = Model()
+    if (ckptPt == None):
+        ckptLs = os.listdir('{}/'.format(ckptDir)) 
+        ckptPt = len(ckptLs) - 1
+    ckptLoadPath = os.path.join(ckptDir, 'checkpoint_{}'.format(ckptPt))
+    model.load_state_dict(torch.load(ckptLoadPath)) 
+    logPrint('Model loaded from Epoch #{}.'.format(ckptPt))
+
+    # Load Data
+    dataVal = validationData(scale=scale, reduction=reduction)
+    logPrint('Data Loaded. Quantity of Validation Instances:{}.'.format(len(dataVal)))
+
+    # Meters
+    modelMeter = AvgMeter()
+    baselineMeter = AvgMeter()
+    lossMeter = AvgMeter()
+
+    # Translator
+    numpyTrans = ToNumpy()
+
+    # Loss Function
+    lossHandle = nn.L1Loss()
+
+    logPrint('Evaluating...')
+    model.eval()
+
+    # Evaluation
+    for _, sample in enumerate(dataVal):
+
+        # Prepare & Feed
+        imgGt, imgInput = sample["groundTruth"], sample["input"] 
+        with torch.no_grad():
+            imgOutput = model(imgInput)
+        
+        # Compute Baseline
+        imgBaseline = F.interpolate(imgInput, scale_factor=scale, mode='bicubic', align_corners=True)
+        lossMeter.append(lossHandle(imgGt, imgBaseline))
+
+        # Translate
+        imgGt, imgOutput, imgBaseline = numpyTrans([imgGt, imgOutput, imgBaseline])
+        
+        # Compute Metrics
+        psnrModel = PSNR(imgGt, imgOutput)
+        psnrBaseline = PSNR(imgGt, imgBaseline)
+        modelMeter.append(psnrModel)
+        baselineMeter.append(psnrBaseline)
+    
+    # Evaluation Outcome 
+    logPrint("Loss of the Baseline:{:.4f}".format(lossMeter.movingAvg()))  
+    logPrint("Avg PSNR:\nModel vs.Baseline\n{:.4f}dB {:.4f}dB".format(modelMeter.movingAvg(), baselineMeter.movingAvg()))  
+
+
+if __name__ == '__main__': 
+    main()
